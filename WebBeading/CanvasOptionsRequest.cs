20d56a622d0899f172de2b20c8622a3b4c98911a
Izeren <yushalnik@bk.ru>
2016-09-27 15:20:19 +0300

    Added start test page

---
+++ WebBeading/CanvasOptionsRequest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBeading
+{
+    public class CanvasOptionsRequest
+    {
+        public int width;
+        public int height;
+        public int bixelWidth;
+        public int bixelHeight;
+
+        public void setWidth(int newWidth)
+        {
+            width = newWidth;
+        }
+
+        public void setHeight(int newHeight)
+        {
+            height = newHeight;
+        }
+
+        public void setBixelWidth(int newBixelWidth)
+        {
+            bixelWidth = newBixelWidth;
+        }
+
+        public void setBixelHeight(int newBixelHeight)
+        {
+            bixelHeight = newBixelHeight;
+        }
+
+        public int getWidth()
+        {
+            return width;
+        }
+        public int getHeight()
+        {
+            return height;
+        }
+        public int getBixelWidth()
+        {
+            return bixelWidth;
+        }
+        public int getBixelHeight()
+        {
+            return bixelHeight;
+        }
+    }
+}
---
+++ WebBeading/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.ModelBinding;
+
+namespace WebBeading
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                CanvasOptionsRequest request = new CanvasOptionsRequest();
+
+                if (TryUpdateModel(request, new FormValueProvider(ModelBindingExecutionContext)))
+                {
+                    Console.WriteLine("Hello world!\n");
+                }
+            }
+        }
+    }
+}
